aa9701eb895594a926737af8b25efe09c2ade827
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-10 17:51:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,10 +129,10 @@ def shoe_size(players_name)
     end
   end
 end
-# def team_colors(team_name)
-#   game_hash.each do |place, team|
-#     if team[:team_name] == team_name
-#       return team[:colors]
-#     end
-#   end
-# end
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    if team[:team_name] == team_name
+      return team[:colors]
+    end
+  end
+end
